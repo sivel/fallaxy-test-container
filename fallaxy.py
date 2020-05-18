003450f19371cb2c0b1b22ff8e10e43005f763f3
Matt Martz <matt@sivel.net>
2020-05-18 10:41:27 -0500

    Really don't run threaded

--- fallaxy.py
+++ fallaxy.py
@@ -831,7 +831,7 @@ def main():
 
     start_up()
 
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False, threaded=False)
 
 
 if __name__ == '__main__':
